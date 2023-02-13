a4057dfba7fd989d8d5ed9a7c5beff7dc353337d
alexander <farbantis@gmail.com>
2023-02-13 19:17:30 +0200

    started using Factories

--- django_module/settings.py
+++ django_module/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'factory',
     'shop.apps.ShopConfig',
 ]
 
--- shop/tests.py
+++ shop/tests.py
@@ -1,3 +1,4 @@
+import factory
 from django.contrib.auth import authenticate
 from django.test import TestCase, RequestFactory
 from django.urls import reverse
@@ -5,23 +6,39 @@ from .models import Merchandise, Order, ShopBuyer
 from .views import OrderHistoryView
 
 
+class ShopBuyerFactory(factory.django.DjangoModelFactory):
+    username = 'testuser'
+    password = factory.PostGenerationMethodCall('set_password', 'testpassword')
+    email = factory.Faker('email')
+    wallet = 200
+
+    class Meta:
+        model = ShopBuyer
+
+
+class MerchandiseFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Merchandise
+    name = factory.Faker('name')
+    description = factory.Faker('text')
+    price = 10
+    picture = '/media/pictures/2023/02/airpods.jpg'
+    stock = 100
+
+
+class OrderFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Order
+    merchandise = factory.SubFactory(MerchandiseFactory)
+    buyer = factory.SubFactory(ShopBuyerFactory)
+    order_quantity = 20
+
+
 class OrderHistoryTests(TestCase):
     def setUp(self) -> None:
-        product1 = Merchandise.objects.create(
-            name='item1',
-            description='item1-desc',
-            price=10,
-            picture='/media/pictures/2023/02/airpods.jpg',
-            stock=100
-        )
-        product2 = Merchandise.objects.create(
-            name='item2',
-            description='item2-desc',
-            price=20,
-            picture='/media/pictures/2023/02/airpods1.jpg',
-            stock=200
-        )
-        self.user = ShopBuyer.objects.create(username='testuser', password='testuserpassword', wallet=200)
+        product1 = MerchandiseFactory(name='item1')
+        product2 = MerchandiseFactory(name='item2')
+        self.user = ShopBuyerFactory()
         Order.objects.create(
             merchandise_id=product1.id,
             buyer_id=self.user.id,
@@ -47,26 +64,16 @@ class OrderHistoryTests(TestCase):
 
 class ProductViewTests(TestCase):
     def setUp(self):
-        self.username = 'testuser'
-        self.password = 'testpassword'
+        self.shop_buyer = ShopBuyerFactory()
+        self.product = MerchandiseFactory()
         self.wallet = 200
         self.stock = 100
-        self.price = 10
         self.order_quantity = 20
-        self.shop_buyer = ShopBuyer.objects.create_user(
-            username=self.username,
-            password=self.password,
-            wallet=self.wallet)
-        self.product = Merchandise.objects.create(
-            name='Test Product',
-            price=self.price,
-            stock=self.stock,
-            picture='/media/pictures/2023/02/airpods.jpg')
+        #self.order = OrderFactory()
         self.order = Order.objects.create(
             merchandise_id=self.product.id,
             buyer_id=self.shop_buyer.id,
             order_quantity=self.order_quantity)
-        self.user = ShopBuyer.objects.get(username=self.username)
 
     def tearDown(self) -> None:
         self.order.delete()
@@ -87,15 +94,10 @@ class ProductViewTests(TestCase):
     def test_is_enough_money(self):
         self.assertTrue(self.order.is_enough_money)
 
-    def test_order_object_creation(self):
-        self.assertEqual(Order.objects.count(), 1)
-        self.assertEqual(self.order.order_quantity, self.order_quantity)
-        self.assertEqual(self.product.stock, self.stock)
-
     # ? тупой тест...
     def test_is_money_deduction_after_order(self):
-        self.user.wallet -= self.order.get_order_value
-        self.assertEqual(self.user.wallet, self.wallet - self.order.get_order_value)
+        self.shop_buyer.wallet -= self.order.get_order_value
+        self.assertEqual(self.shop_buyer.wallet, self.wallet - self.order.get_order_value)
 
     def test_is_stock_decreases_after_order(self):
         self.product.stock -= self.order.order_quantity
@@ -104,37 +106,22 @@ class ProductViewTests(TestCase):
 
 class LoginViewTests(TestCase):
     def setUp(self):
-        self.factory = RequestFactory
-        self.username = 'testuser'
         self.password = 'testpassword'
-        self.user = ShopBuyer.objects.create_user(username=self.username, password=self.password,
-                                                  email='test@example.com', wallet=100)
+        self.user = ShopBuyerFactory()
 
     def tearDown(self) -> None:
         self.user.delete()
 
-    def test_correct(self):
-        user = authenticate(username=self.username, password=self.password)
-        self.assertTrue((user is not None) and user.is_authenticated)
-
     def test_is_superuser_or_is_staff(self):
-        user = authenticate(username=self.username, password=self.password)
+        user = authenticate(username=self.user.username, password=self.password)
         self.assertFalse(user.is_superuser and user.is_staff)
 
-    def test_wrong_username(self):
-        user = authenticate(username='wrong', password=self.password)
-        self.assertFalse((user is not None) and user.is_authenticated)
-
-    def test_wrong_password(self):
-        user = authenticate(username=self.username, password='wrong')
-        self.assertFalse((user is not None) and user.is_authenticated)
-
     def test_normal_user_redirect(self):
-        response = self.client.post(reverse('shop:login'), {'username': self.username, 'password': self.password})
+        response = self.client.post(reverse('shop:login'), {'username': self.user.username, 'password': self.password})
         self.assertRedirects(response, reverse('shop:index'))
 
     def test_staff_user_redirect(self):
         self.user.is_staff = True
         self.user.save()
-        response = self.client.post(reverse('shop:login'), {'username': self.username, 'password': self.password})
+        response = self.client.post(reverse('shop:login'), {'username': self.user.username, 'password': self.password})
         self.assertRedirects(response, reverse('shop:review_merchandise'))
